2a763a8b29b53adbda8087b92ddd9fe460c87de3
Mitchell Mirano <mitchellmirano25@gmail.com>
2022-12-01 22:54:58 -0500

    integrate2d methods

--- src/NumericalMethods.jl
+++ src/NumericalMethods.jl
@@ -1,20 +1,28 @@
 module NumericalMethods
 
-    # interpolation methods
-    include("./interpolate/lagrange.jl")
-    include("interpolate/newton.jl")
+       # interpolation methods
+       include("./interpolate/lagrange.jl")
+       include("./interpolate/newton.jl")
 
-    export lagrange
-           newton
+       export lagrange
+              newton
 
-    # integral methods
-    include("./integral/trapeze.jl")
-    include("./integral/simpson.jl")
-    include("./integral/newton-cotes.jl")
-    include("./integral/romberg.jl")
-    
-    export trapeze
-           simpson
-           newtonCotes
-           romberg
+       # integral methods
+       include("./integral/trapeze.jl")
+       include("./integral/simpson.jl")
+       include("./integral/newton-cotes.jl")
+       include("./integral/romberg.jl")
+       
+       export trapeze
+              simpson
+              newtonCotes
+              romberg
+
+       # integral2d methods
+       include("./integral2d/trapezed2.jl")
+       include("./integral2d/simpson2d.jl")
+
+       export trapeze2d
+              simpson2d
+
 end # module NumericalMethods
